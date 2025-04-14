263abcc4e6e93c2297d5bcc83f4d2d0aeba5ee99
Noah Funganoti <66160458+Michaelfun@users.noreply.github.com>
2025-04-14 16:38:59 +0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Add your project directory to the Python path
-path = '/home/your_username/pregnancy_modal'  # Replace 'your_username' with your PythonAnywhere username
-if path not in sys.path:
-    sys.path.append(path)
-
-from api import app as application 
